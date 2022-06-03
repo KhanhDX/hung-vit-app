0599624b0ef9632547284d9839bd8779c72d1c00
KHANH DANG XUAN <ntt.dang_xuan@extaccount.com>
2022-06-03 13:36:42 +0700

    c

--- src/main/java/com/khanhdx/hihi/Controller.java
+++ src/main/java/com/khanhdx/hihi/Controller.java
@@ -3,11 +3,13 @@ package com.khanhdx.hihi;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin("*")
 @RequestMapping("/khanhdx")
 public class Controller {
     @GetMapping("/hello")
